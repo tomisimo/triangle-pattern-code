035d0ab07f3e1240d2576feab97d3ebe6fbc8b64
Simo Tamas <simo_tamas2007@yahoo.com>
2022-09-13 19:32:10 +0300

    TRIUNGHI DESCENDENT DE 10 RANDURI 10 COLOANE

--- TRIANGLEpattern/src/tamas/Main.java
+++ TRIANGLEpattern/src/tamas/Main.java
@@ -5,42 +5,42 @@ public class Main {
     public static void main(String[] args) {
 
         int numarRepetari = 5;
-        for (int numarDeInceputRandGol = 1 ; numarDeInceputRandGol <= numarRepetari ; numarDeInceputRandGol++){ //CATE RANDURI DE STELE SA FIE
-            for (int numarDeInceputStele = 1 ; numarDeInceputStele <= numarRepetari ; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
+        for (int numarDeInceputRandGol = 1; numarDeInceputRandGol <= numarRepetari; numarDeInceputRandGol++) { //CATE RANDURI DE STELE SA FIE
+            for (int numarDeInceputStele = 1; numarDeInceputStele <= numarRepetari; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
                 System.out.print(" * ");
             }
             System.out.println();//FUNCTIA PRINT RAND GOL TRECE LA PRINTAREA URMATORULUI RAND DE STELE
         }
 
-        for (int numarDeInceputRandGol = 1 ; numarDeInceputRandGol <= numarRepetari ; numarDeInceputRandGol++){ //CATE RANDURI DE STELE SA FIE
-            for (int numarDeInceputStele = 1 ; numarDeInceputStele <= numarDeInceputRandGol ; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
+        for (int numarDeInceputRandGol = 1; numarDeInceputRandGol <= numarRepetari; numarDeInceputRandGol++) { //CATE RANDURI DE STELE SA FIE
+            for (int numarDeInceputStele = 1; numarDeInceputStele <= numarDeInceputRandGol; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
                 System.out.print(" * ");
             }
             System.out.println();//FUNCTIA PRINT RAND GOL TRECE LA PRINTAREA URMATORULUI RAND DE STELE
         }
 
-        for (int numarDeInceputRandGol = 1 ; numarDeInceputRandGol <= numarRepetari ; numarDeInceputRandGol++){ //CATE RANDURI DE STELE SA FIE
-            for (int numarDeInceputStele = numarDeInceputRandGol ; numarDeInceputStele <= numarRepetari ; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
+        for (int numarDeInceputRandGol = 1; numarDeInceputRandGol <= numarRepetari; numarDeInceputRandGol++) { //CATE RANDURI DE STELE SA FIE
+            for (int numarDeInceputStele = numarDeInceputRandGol; numarDeInceputStele <= numarRepetari; numarDeInceputStele++) { //CATE COLOANE DE STELE SA FIE
                 System.out.print(" * ");
             }
             System.out.println();//FUNCTIA PRINT RAND GOL TRECE LA PRINTAREA URMATORULUI RAND DE STELE
         }
 
-        for (int numarDeInceputRandGol = 1 ; numarDeInceputRandGol <= numarRepetari ; numarDeInceputRandGol++){
-            for (int numarDeInceputSaptiuGol = numarDeInceputRandGol ; numarDeInceputSaptiuGol <= numarRepetari ; numarDeInceputSaptiuGol++){
+        for (int numarDeInceputRandGol = 1; numarDeInceputRandGol <= numarRepetari; numarDeInceputRandGol++) {
+            for (int numarDeInceputSaptiuGol = numarDeInceputRandGol; numarDeInceputSaptiuGol <= numarRepetari; numarDeInceputSaptiuGol++) {
                 System.out.print("  ");
             }
-            for (int numarDeInceputSpatiuStea = 1 ; numarDeInceputSpatiuStea <= numarDeInceputRandGol ; numarDeInceputSpatiuStea++){
+            for (int numarDeInceputSpatiuStea = 1; numarDeInceputSpatiuStea <= numarDeInceputRandGol; numarDeInceputSpatiuStea++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-        for (int numarDeInceputRandGol = 1 ; numarDeInceputRandGol <= numarRepetari ; numarDeInceputRandGol++){
-            for (int numarDeInceputSpatiuGol = 1 ; numarDeInceputSpatiuGol <= numarDeInceputRandGol ; numarDeInceputSpatiuGol++){
+        for (int numarDeInceputRandGol = 1; numarDeInceputRandGol <= numarRepetari; numarDeInceputRandGol++) {
+            for (int numarDeInceputSpatiuGol = 1; numarDeInceputSpatiuGol <= numarDeInceputRandGol; numarDeInceputSpatiuGol++) {
                 System.out.print("  ");
             }
-            for (int numarDeInceputSpatiuStea = numarDeInceputRandGol ; numarDeInceputSpatiuStea <= numarRepetari ; numarDeInceputSpatiuStea++){
+            for (int numarDeInceputSpatiuStea = numarDeInceputRandGol; numarDeInceputSpatiuStea <= numarRepetari; numarDeInceputSpatiuStea++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -48,9 +48,9 @@ public class Main {
         System.out.println("---------------------------------------------------------------------------");
         System.out.println();
 
-        int rowNumbers  = 5;
-        for (int rowI = 1 ; rowI <= rowNumbers ; rowI++){
-            for (int starJ = 1 ; starJ <= rowI ; starJ++){
+        int rowNumbers = 5;
+        for (int rowI = 1; rowI <= rowNumbers; rowI++) {
+            for (int starJ = 1; starJ <= rowI; starJ++) {
                 System.out.print(" * ");
             }
             System.out.println();
@@ -58,9 +58,9 @@ public class Main {
         System.out.println("triangle increasing");
         System.out.println();
 
-        int rowNumber= 5;
-        for (int rowI = 1 ; rowI <= rowNumber ; rowI++){
-            for (int starJ = rowI ; starJ <= rowNumber ; starJ++){
+        int rowNumber = 5;
+        for (int rowI = 1; rowI <= rowNumber; rowI++) {
+            for (int starJ = rowI; starJ <= rowNumber; starJ++) {
                 System.out.print(" * ");
             }
             System.out.println();
@@ -69,18 +69,18 @@ public class Main {
 
         System.out.println("EDUCABA STAR PATTERN CODES");
         int myRows = 10;
-        for (int i = 1; i <= myRows; i++){ //randuri de la 1 la 10 bucati
-            for (int j = 1; j <= i; j++){  //coloane de la stanga la dreapta de la 10 la 1 bucati
+        for (int i = 1; i <= myRows; i++) { //randuri de la 1 la 10 bucati
+            for (int j = 1; j <= i; j++) {  //coloane de la stanga la dreapta de la 10 la 1 bucati
                 System.out.print(" * ");
             }
             System.out.println("   ");
         }
-
-
-
-
-
-
-
+        int myRow = 10;
+        for (int i = 1; i <= myRow; i++) { // randuri 10
+            for (int j = myRow; j > i; j--) { // coloane 10
+                System.out.print(" * ");
+            }
+            System.out.println("   ");
+        }
     }
 }
